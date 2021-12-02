be100a23896e1a3d2d467cf70ce5aa242bed232e
German Osin <german.osin@gmail.com>
2021-12-02 14:52:14 +0300

    Added microservices paths, http path and value escaping

---
+++ src/test/java/org/opendatadiscovery/oddrn/model/DockerMicroservicePathTest.java
@@ -0,0 +1,18 @@
+package org.opendatadiscovery.oddrn.model;
+
+import org.junit.jupiter.api.Test;
+import org.opendatadiscovery.oddrn.AbstractGeneratorTest;
+
+public class DockerMicroservicePathTest extends AbstractGeneratorTest {
+    @Test
+    public void shouldGenerateImagePath() throws Exception {
+        shouldGeneratePath(
+            DockerMicroservicePath.builder()
+                .image("ghcr.io/opendatadiscovery/odd-platform:latest")
+                .build(),
+            "image",
+            "//microservice/docker/image/ghcr.io\\\\opendatadiscovery\\\\odd-platform:latest"
+        );
+    }
+
+}
